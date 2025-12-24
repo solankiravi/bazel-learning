90c85c0f68d07717d9175c1778117d9eedde2ad4
Ravi Kumar <ravikumarsinghsolanki@gmail.com>
2025-12-24 21:40:22 +0530

    Deep dive into testing

---
+++ tests/test_math_integration.py
@@ -0,0 +1,11 @@
+from lib.math_utils import add, substract, multiply, divide
+import unittest
+import time
+
+class TestMathUtils(unittest.TestCase):
+    def test_add(self):
+        time.sleep(10)# Simulating a long-running test
+        assert add(3,4) == 7
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ tests/test_math_unit.py
@@ -0,0 +1,18 @@
+from lib.math_utils import add, substract, multiply, divide
+import unittest
+
+class TestMathUtils(unittest.TestCase):
+    def test_add(self):
+        assert add(3,4) == 7
+
+    def test_sub(self):
+        assert substract(3,4) == -1
+
+    def test_mul(self):
+        assert multiply(3,4) == 12
+
+    def test_divide(self):
+        assert divide(3,4) == 0.75
+
+if __name__ == '__main__':
+    unittest.main()
